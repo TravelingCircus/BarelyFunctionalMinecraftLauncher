b64ff6b47b25fbb8bc8d8b0d4a98c27ce8b1fe51
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-09-17 20:33:25 +0200

    imported minecraft utils lib

--- BFML/MainWindow.xaml.cs
+++ BFML/MainWindow.xaml.cs
@@ -1,28 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using CmlLib.Core;
+using CmlLib.Core.Auth;
+using CmlLib.Core.Version;
 
 namespace BFML
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            LaunchMinecraft();
+        }
+
+        private async void LaunchMinecraft()
+        {
+            System.Net.ServicePointManager.DefaultConnectionLimit = 256;
+
+            MinecraftPath path = new MinecraftPath();
+            CMLauncher launcher = new CMLauncher(path);
+
+            launcher.FileChanged += (e) =>
+            {
+                Console.WriteLine("[{0}] {1} - {2}/{3}", e.FileKind.ToString(), e.FileName, e.ProgressedFileCount, e.TotalFileCount);
+            };
+            launcher.ProgressChanged += (s, e) =>
+            {
+                Console.WriteLine("{0}%", e.ProgressPercentage);
+            };
+
+            MVersionCollection versions = await launcher.GetAllVersionsAsync();
+            foreach (MVersionMetadata v in versions)
+            {
+                Console.WriteLine(v.Name);
+            }
+
+            Process process = await launcher.CreateProcessAsync("1.16.5", new MLaunchOption
+            {
+                MaximumRamMb = 2048,
+                Session = MSession.GetOfflineSession("hello123"),
+            });
+
+            process.Start();
         }
     }
 }
