0fef7dc504c8d3cc778643309cd56a7ae66f88a8
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-09-21 20:57:05 +0200

    Handler plug

---
+++ HTTPFileServer/MessageHandlers/RegistrationHandler.cs
@@ -0,0 +1,21 @@
+﻿using CommonData.Network;
+
+namespace HTTPFileServer.MessageHandlers;
+
+public class RegistrationHandler : MessageHandler
+{
+    public override bool CanHandle(Message message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Handle(Message message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Message GetResponse(Message message)
+    {
+        throw new NotImplementedException();
+    }
+}
