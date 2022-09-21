785ea145f9d365dff01665addbb773aa31b25c4f
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-09-21 20:52:00 +0200

    Progress on messages

--- Common/NetworkChannel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CommonData;
-
-public class NetworkChannel
-{
-    //public Task SendMessage(Message message);
-    //public Task<Message> ListenForMessage();
-
-    //private void PickHandler();
-}
