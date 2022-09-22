1f30fe23313a0be0f84af3c5ec1c0ea1eca7b32e
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-09-22 18:28:28 +0200

    Updated base message and Registration message

--- Common/Network/Messages/RegistrationRequest.cs
+++ Common/Network/Messages/RegistrationRequest.cs
@@ -4,8 +4,9 @@ namespace CommonData.Network.Messages;
 
 public class RegistrationRequest : Message
 {
-    public readonly string NickName;
-    public readonly string PasswordHash;
+    public const byte Key = 1;
+    public string NickName;
+    public string PasswordHash;
 
     public RegistrationRequest(string nickName, string passwordHash)
     {
@@ -13,21 +14,41 @@ public class RegistrationRequest : Message
         PasswordHash = passwordHash;
     }
 
-    public override byte[] GetHeader()
+    public override MessageHeader GetHeader()
     {
-        throw new NotImplementedException();
+        return new MessageHeader(Key, 2 * sizeof(int)
+        +NickName.Length * sizeof(char)
+        +PasswordHash.Length * sizeof(char));
     }
 
-    public override Stream GetData()
+    protected override Stream GetData()
     {
         MemoryStream buffer = new MemoryStream();
-        buffer.Write(Encoding.UTF8.GetBytes(NickName));
-        buffer.Write(Encoding.UTF8.GetBytes(PasswordHash));
+        byte[] nicknameBytes = Encoding.UTF8.GetBytes(NickName);
+        byte[] passwordBytes = Encoding.UTF8.GetBytes(PasswordHash);
+        
+        buffer.Write(BitConverter.GetBytes(nicknameBytes.Length));
+        buffer.Write(nicknameBytes);
+        buffer.Write(BitConverter.GetBytes(passwordBytes.Length));
+        buffer.Write(passwordBytes);
         return buffer;
     }
 
-    public override Message FromData(StreamReader reader)
+    public override void FromData(Stream stream)
     {
-        throw new NotImplementedException();
+        byte[] intBuffer = new byte[4];
+        byte[] stringBuffer;
+        
+        stream.Read(intBuffer, 0, 4);
+        int stringLength = BitConverter.ToInt32(intBuffer, 0);
+        stringBuffer = new byte[stringLength];
+        stream.Read(stringBuffer, 0, stringLength);
+        NickName = Encoding.UTF8.GetString(stringBuffer);
+        
+        stream.Read(intBuffer, 0, 4);
+        stringLength = BitConverter.ToInt32(intBuffer, 0);
+        stringBuffer = new byte[stringLength];
+        stream.Read(stringBuffer, 0, stringLength);
+        PasswordHash = Encoding.UTF8.GetString(stringBuffer);
     }
 }
