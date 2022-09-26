206d5db873b5721fe87c4fd13fd45230a001ea01
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-09-26 21:56:50 +0200

    Update MessageHandler.cs

--- TCPFileServer/MessageHandlers/MessageHandler.cs
+++ TCPFileServer/MessageHandlers/MessageHandler.cs
@@ -2,7 +2,11 @@
 
 public abstract class MessageHandler
 {
-    public abstract bool CanHandle(MessageHeader messageHeader);
+    public bool CanHandle(MessageHeader messageHeader)
+    {
+        return MessageRegistry.GetMessageTypeName(messageHeader) == GetType().Name;
+    }
+    
     public abstract Task Handle(Stream dataStream);
     public abstract Task<Message> GetResponse(Stream dataStream);
 }
