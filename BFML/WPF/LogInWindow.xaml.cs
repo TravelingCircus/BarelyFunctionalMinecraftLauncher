ef6836b7bce13d161ff158188e93af50ec2aafb5
Jony_Tough <xp.drake@gmail.com>
2022-09-27 22:28:15 +0300

    LogIn window Field and Close/Minimize

--- BFML/WPF/LogInWindow.xaml.cs
+++ BFML/WPF/LogInWindow.xaml.cs
@@ -23,5 +23,41 @@ namespace BFML.WPF
         {
             InitializeComponent();
         }
+
+        private void ShutDown(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                App.Current.Shutdown();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Minimize(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.WindowState = WindowState.Minimized;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void MoveWindow(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                if (this.WindowState == WindowState.Maximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    Application.Current.MainWindow.Top = 3;
+                }
+                this.DragMove();
+            }
+        }
     }
 }
