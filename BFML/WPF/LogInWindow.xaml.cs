4ea5e3bb0de7aedb7c1cbb115aca9208dfa54737
WingedCryptid <yariksouth@gmail.com>
2022-09-30 23:14:28 +0300

    ...

--- BFML/WPF/LogInWindow.xaml.cs
+++ BFML/WPF/LogInWindow.xaml.cs
@@ -11,9 +11,6 @@ using TCPFileClient;
 
 namespace BFML.WPF
 {
-    /// <summary>
-    /// Interaction logic for LogInWindow.xaml
-    /// </summary>
     public partial class LogInWindow : Window
     {
         private readonly FileClient _fileClient;
@@ -66,84 +63,80 @@ namespace BFML.WPF
 
         private async void RegisterButtonOnClick(object sender, RoutedEventArgs e)
         {
-            //string nickname = NicknameBar.Text;
-            //string password = PasswordBar.Text;
+            string nickname = InputNickname.Text;
+            string password = InputPassword.Text;
 
-            //User newUser = new User(nickname, password);
-            //RegistrationResponse response = await _fileClient.SendRegistrationRequest(newUser);
-            //if (!response.Success)
-            //{
-            //    //TODO Display that user exists
-            //    return;
-            //}
+            User newUser = new User(nickname, password);
+            RegistrationResponse response = await _fileClient.SendRegistrationRequest(newUser);
+            if (!response.Success)
+            {
+                //TODO Display that user exists
+                InputNickname.Text = "";
+                InputPassword.Text = "";
+                return;
+            }
 
-            //LocalPrefs.SaveLocalPrefs(nickname, password);
-            //LocalPrefs localPrefs = LocalPrefs.GetLocalPrefs();
-            //MainWindow mainWindow = new MainWindow(_fileClient, newUser, localPrefs, _launchConfiguration, _version);
+            LocalPrefs.SaveLocalPrefs(nickname, password);
+            LocalPrefs localPrefs = LocalPrefs.GetLocalPrefs();
+            MainWindow mainWindow = new MainWindow(_fileClient, newUser, localPrefs, _launchConfiguration, _version);
+            mainWindow.Show();
+            Close();
         }
 
         private async void LogInButtonOnClick(object sender, RoutedEventArgs e)
         {
-            //string nickname = NicknameBar.Text;
-            //string password = PasswordBar.Text;
+            string nickname = InputNickname.Text;
+            string password = InputPassword.Text;
 
-            //User newUser = new User(nickname, password);
-            //LoginResponse response = await _fileClient.SendLoginRequest(newUser);
-            //if (!response.Success)
-            //{
-            //    //TODO Display that user doesn't exists
-            //    return;
-            //}
-
-            //LocalPrefs.SaveLocalPrefs(nickname, password);
-            //LocalPrefs localPrefs = LocalPrefs.GetLocalPrefs();
-            //MainWindow mainWindow = new MainWindow(_fileClient, newUser, localPrefs, _launchConfiguration, _version);
-        }
-
-        public LogInWindow()
-        {
-        }
-
-        private void DebugButtonOnClick(object sender, RoutedEventArgs e)
-        {
-
-            //string password = PasswordBar.Text;
-        }
-
-        private void textNickname_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            inputNickname.Focus();
-        }
-
-        private void inputNickname_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(inputNickname.Text) && inputNickname.Text.Length > 0)
+            User newUser = new User(nickname, password);
+            LoginResponse response = await _fileClient.SendLoginRequest(newUser);
+            if (!response.Success)
             {
-                textNickname.Visibility = Visibility.Collapsed;
+                //TODO Display that user doesn't exists
+                InputNickname.Text = "";
+                InputPassword.Text = "";
+                return;
+            }
+
+            LocalPrefs.SaveLocalPrefs(nickname, password);
+            LocalPrefs localPrefs = LocalPrefs.GetLocalPrefs();
+            MainWindow mainWindow = new MainWindow(_fileClient, newUser, localPrefs, _launchConfiguration, _version);
+            mainWindow.Show();
+            Close();
+        }
+
+        private void TextNicknameMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            InputNickname.Focus();
+        }
+
+        private void InputNicknameTextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(InputNickname.Text) && InputNickname.Text.Length > 0)
+            {
+                TextNickname.Visibility = Visibility.Collapsed;
             }
             else
             {
-                textNickname.Visibility = Visibility.Visible;
+                TextNickname.Visibility = Visibility.Visible;
             }
         }
 
-        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        private void TextPasswordMouseDown(object sender, MouseButtonEventArgs e)
         {
-            inputPassword.Focus();
+            InputPassword.Focus();
         }
 
-        private void inputPassword_PasswordChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        private void InputPasswordPasswordChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
-            if (!string.IsNullOrEmpty(inputPassword.Text) && inputPassword.Text.Length > 0)
+            if (!string.IsNullOrEmpty(InputPassword.Text) && InputPassword.Text.Length > 0)
             {
-                textPassword.Visibility = Visibility.Collapsed;
+                TextPassword.Visibility = Visibility.Collapsed;
             }
             else
             {
-                textPassword.Visibility = Visibility.Visible;
+                TextPassword.Visibility = Visibility.Visible;
             }
-
-
         }
 
     }
