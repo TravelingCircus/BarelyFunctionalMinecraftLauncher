653f63f15793e7577fd7fa80824c0bc225c8a9ce
WingedCryptid <yariksouth@gmail.com>
2022-10-08 19:24:52 +0300

    Fixed obj renderer

--- BFML/3D/ObjLineParser.cs
+++ BFML/3D/ObjLineParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace ABOBAEngine.Rendering.Models;
@@ -27,7 +28,7 @@ public abstract class ObjLineParser
 public sealed class VertexObjLineParser : ObjLineParser
 {
     public readonly List<float> Vertices = new List<float>();
-    private readonly char[] _pattern = { 'v', ' ' };
+    private readonly char[] _pattern = {'v', ' '};
 
     public override char[] Pattern() => _pattern;
 
@@ -41,12 +42,12 @@ public sealed class VertexObjLineParser : ObjLineParser
         for (int i = 0; i < 2; i++)
         {
             while (span[position] != ' ') position++;
-            Vertices.Add(float.Parse(span.Slice(0, position)));
+            Vertices.Add(float.Parse(span.Slice(0, position), NumberStyles.Any, CultureInfo.InvariantCulture));
             span = span.Slice(++position);
             position = 0;
         }
 
-        Vertices.Add(float.Parse(span));
+        Vertices.Add(float.Parse(span, NumberStyles.Any, CultureInfo.InvariantCulture));
     }
 }
 
@@ -55,7 +56,7 @@ public sealed class FaceObjLineParser : ObjLineParser
     public readonly List<uint> VertexIndices = new List<uint>();
     public readonly List<uint> NormalIndices = new List<uint>();
     public readonly List<uint> UVIndices = new List<uint>();
-    private readonly char[] _pattern = { 'f', ' ' };
+    private readonly char[] _pattern = {'f', ' '};
 
     public override char[] Pattern() => _pattern;
 
@@ -68,12 +69,12 @@ public sealed class FaceObjLineParser : ObjLineParser
             while (span[position] != ' ') position++;
             span = span.Slice(position + 1);
             position = 0;
-            
+
             while (span[position] != '/') position++;
             VertexIndices.Add(uint.Parse(span.Slice(0, position)) - 1);
             span = span.Slice(++position);
             position = 0;
-            
+
             while (span[position] != '/') position++;
             UVIndices.Add(uint.Parse(span.Slice(0, position)) - 1);
             span = span.Slice(++position);
@@ -81,7 +82,7 @@ public sealed class FaceObjLineParser : ObjLineParser
 
             while (position < span.Length && span[position] != ' ') position++;
             NormalIndices.Add(uint.Parse(span.Slice(0, position)) - 1);
-            if(position <= span.Length)span = span.Slice(position);
+            if (position <= span.Length) span = span.Slice(position);
             position = 0;
         }
     }
@@ -90,7 +91,7 @@ public sealed class FaceObjLineParser : ObjLineParser
 public sealed class AlbedoUVObjLineParser : ObjLineParser
 {
     public readonly List<float> AlbedoUVs = new List<float>();
-    private readonly char[] _pattern = { 'v', 't', ' ' };
+    private readonly char[] _pattern = {'v', 't', ' '};
 
     public override char[] Pattern() => _pattern;
 
@@ -103,9 +104,9 @@ public sealed class AlbedoUVObjLineParser : ObjLineParser
         position = 0;
 
         while (span[position] != ' ') position++;
-        AlbedoUVs.Add(float.Parse(span.Slice(0, position)));
+        AlbedoUVs.Add(float.Parse(span.Slice(0, position), NumberStyles.Any, CultureInfo.InvariantCulture));
         span = span.Slice(++position);
 
-        AlbedoUVs.Add(float.Parse(span));
+        AlbedoUVs.Add(float.Parse(span, NumberStyles.Any, CultureInfo.InvariantCulture));
     }
 }
