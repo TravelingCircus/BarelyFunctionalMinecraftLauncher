6c2d90f42777f08b1d2dfcfc130e9cf341f8d002
WingedCryptid <yariksouth@gmail.com>
2022-10-08 20:27:01 +0300

    Added DataHash class

---
+++ Common/DataHash.cs
@@ -0,0 +1,18 @@
+﻿namespace CommonData;
+
+public static class DataHash
+{
+    public static int ReadHashFromFiles(string path)
+    {
+        DirectoryInfo directoryInfo = new DirectoryInfo(path);
+        FileInfo[] files = directoryInfo.GetFiles();
+        int hashCode = 0;
+        foreach(FileInfo file in files)
+        {
+            hashCode += file.Name.GetHashCode();
+            hashCode /=  2;
+        }
+
+        return hashCode;
+    }
+}
