f26c747dfd6a9b163cceaa4d418b7421fc3364f7
Jony_Tough <xp.drake@gmail.com>
2022-10-12 21:01:02 +0300

    LoadingWindow/unfinished bar(sorry)

---
+++ BFML/ProgressBar.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace BFML.WPF
+{
+    public partial class ProgressBar : UserControl
+    {
+        public ProgressBar()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ BFML/WPF/Loading.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace BFML.WPF
+{
+    public partial class Loading : Window
+    {
+        public Loading()
+        {
+            InitializeComponent();
+        }
+    }
+}
