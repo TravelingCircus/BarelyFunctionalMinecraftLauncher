bcaa89765c2d4457be097c174fc979ae6fb169b2
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-10-17 22:19:33 +0200

    set up empty mod project

--- IntegrationMod/src/main/java/com/example/examplemod/ExampleMod.java
+++ IntegrationMod/src/main/java/com/example/examplemod/ExampleMod.java
@@ -18,7 +18,7 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("BFMLIntegration")
+@Mod("bfmlintegration")
 public class ExampleMod
 {
     // Directly reference a slf4j logger
