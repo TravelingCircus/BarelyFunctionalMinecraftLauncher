2f00cc28ce06085910d6017c7bcf213144ed4fe7
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-10-17 21:53:40 +0200

    some set up for integration mod

--- IntegrationMod/src/main/java/com/example/examplemod/ExampleMod.java
+++ IntegrationMod/src/main/java/com/example/examplemod/ExampleMod.java
@@ -18,7 +18,7 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod("BFMLIntegration")
 public class ExampleMod
 {
     // Directly reference a slf4j logger
@@ -47,7 +47,7 @@ public class ExampleMod
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // Some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("BFMLIntegration", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
