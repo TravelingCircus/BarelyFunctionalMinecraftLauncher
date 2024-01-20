3f3dda7f5b4bbffa5819896611aae8ad7fd06567
WingedCryptid <yariksouth@gmail.com>
2024-01-21 01:35:55 +0200

    Added is font installed check.

--- BFML/WPF/FontInstaller.cs
+++ BFML/WPF/FontInstaller.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System.Drawing.Text;
+using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace BFML.WPF;
@@ -8,11 +10,11 @@ public static class FontInstaller
     public static void InstallFont(FileInfo file)
     {
         if (!file.Exists || file.Extension != ".ttf") throw new FileLoadException($"Font {file.FullName} doesn't exist or has wrong extension");
+
+        InstalledFontCollection installedFontCollection = new InstalledFontCollection();
+        if (installedFontCollection.Families.Any(family => family.Name == "Minecraft")) return;
         
-        int result = AddFontResource(file.FullName);
-        
-        //int error = Marshal.GetLastWin32Error();
-        //if (error != 0) throw new Win32Exception(error);
+        int _ = AddFontResource(file.FullName);
     }
     
     [DllImport("gdi32.dll", EntryPoint = "AddFontResourceW", SetLastError = true)]
