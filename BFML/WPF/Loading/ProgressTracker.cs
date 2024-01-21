f2285f0154afbe033fc908f6b3d951211164b566
Maksym Nashyba <nashybamaksym@gmail.com>
2024-01-21 20:23:11 +0100

    #32 updated tracker class

--- BFML/WPF/Loading/ProgressTracker.cs
+++ BFML/WPF/Loading/ProgressTracker.cs
@@ -5,15 +5,18 @@ namespace BFML.WPF.Loading;
 public class ProgressTracker
 {
     public event Action Changed;
+    public event Action Ended;
     public float Current { get; private set; }
     public TrackerState State { get; private set; }
+    public string Label { get; private set; }
     private MeasurementUnits _units;
     private float _maxValue;
 
-    public ProgressTracker(float maxValue, MeasurementUnits units)
+    public ProgressTracker(float maxValue, MeasurementUnits units, string label)
     {
         _maxValue = maxValue;
         _units = units;
+        Label = label;
     }
 
     public void Add(float step)
