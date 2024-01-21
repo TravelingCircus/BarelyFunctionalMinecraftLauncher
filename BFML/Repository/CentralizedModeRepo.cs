2d3ecb5b233e5bd9a871f85d7347ca37f9d1e4ba
WingedCryptid <yariksouth@gmail.com>
2024-01-21 02:53:50 +0200

    Fixed launch bug; Deleted TCPFileServer; Deleted TCPFileClient.

--- BFML/Repository/CentralizedModeRepo.cs
+++ BFML/Repository/CentralizedModeRepo.cs
@@ -1,25 +1,20 @@
-﻿using System.Threading.Tasks;
-using BFML.Core;
+﻿using BFML.Core;
 using BFML.Repository.RepoIOAdapters;
-using Common.Misc;
-using Common.Models;
-using FileClient;
-using Utils;
 
 namespace BFML.Repository;
 
 internal sealed class CentralizedModeRepo : Repo
 {
-    private readonly ServerConnection _serverConnection;
+    //private readonly ServerConnection _serverConnection;
     
-    internal CentralizedModeRepo(RepoIO repoIo, ServerConnection serverConnection) : base(repoIo)
+    internal CentralizedModeRepo(RepoIO repoIo) : base(repoIo) //ServerConnection serverConnection
     {
-        _serverConnection = serverConnection;
+        //_serverConnection = serverConnection;
     }
 
-    internal Task<Result<User>> CreateRecord(User user) => _serverConnection.CreateRecord(user);
+    //internal Task<Result<User>> CreateRecord(User user) => _serverConnection.CreateRecord(user);
 
-    internal Task<Result<User>> Authenticate(User user) => _serverConnection.Authenticate(user);
+    //internal Task<Result<User>> Authenticate(User user) => _serverConnection.Authenticate(user);
 
     protected override bool ForgeFilter(Forge forge)
     {
